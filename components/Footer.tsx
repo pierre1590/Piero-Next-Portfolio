16bc49e4ffd764a0d75c7626069d8aa004238aff
Piero Sabino <piero.sa@icloud.com>
2025-08-21 19:49:33 +0200

    Update project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,20 +7,14 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4 flex flex-col items-center">
         {/* Social media icons */}
         <div className="flex space-x-6 mb-4">
-          <Link href="https://www.linkedin.com/in/pierosabino" passHref legacyBehavior>
-            <a className="text-gray-500 hover:text-blue-600 transition-colors duration-300">
-              <FaLinkedinIn size={24} />
-            </a>
+          <Link href="https://www.linkedin.com/in/pierosabino" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-blue-600 transition-colors duration-300">
+            <FaLinkedinIn size={24} />
           </Link>
-          <Link href="https://www.instagram.com/piero_sabino1" passHref legacyBehavior>
-            <a className="text-gray-500 hover:text-pink-500 transition-colors duration-300">
-              <FaInstagram size={24} />
-            </a>
+          <Link href="https://www.instagram.com/piero_sabino1" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-pink-500 transition-colors duration-300">
+            <FaInstagram size={24} />
           </Link>
-          <Link href="https://github.com/pierre1590" passHref legacyBehavior>
-            <a className="text-gray-500 hover:text-gray-800 transition-colors duration-300">
-              <FaGithub size={24} />
-            </a>
+          <Link href="https://github.com/pierre1590" target="_blank" rel="noopener noreferrer" className="text-gray-500 hover:text-gray-800 transition-colors duration-300">
+            <FaGithub size={24} />
           </Link>
         </div>
 
@@ -29,7 +23,7 @@ const Footer: React.FC = () => {
           <a href="https://www.iubenda.com/privacy-policy/60158333" className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe " title="Privacy Policy ">
             Privacy Policy
           </a>
-          <a href="https://www.iubenda.com/privacy-policy/60158333/cookie-policy" className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe" title="Cookie Policy">
+          <a href="https://www.iubenda.com/privacy/60158333/cookie-policy" className="iubenda-white iubenda-noiframe iubenda-embed iubenda-noiframe" title="Cookie Policy">
             Cookie Policy
           </a>
         </div>
